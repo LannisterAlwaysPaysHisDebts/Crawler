d5e4e71aa496f0e22685ba8f771710e2bd37614d
caoting <307298900@qq.com>
2020-09-04 12:44:49 +0800

    rpc

---
+++ rpc/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	rpcdemo "Crawler/rpc"
+	"fmt"
+	"net"
+	"net/rpc/jsonrpc"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	client := jsonrpc.NewClient(conn)
+
+	var result float64
+	err = client.Call("DemoService.Div", rpcdemo.Args{
+		A: 10,
+		B: 3,
+	}, &result)
+	fmt.Println(result, err)
+
+	err = client.Call("DemoService.Div", rpcdemo.Args{
+		A: 10,
+		B: 0,
+	}, &result)
+	fmt.Println(result, err)
+
+}
